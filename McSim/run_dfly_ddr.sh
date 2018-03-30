4a0a67c7a13da366ed41bd19811b423c318d013f
Sungkeun <ksungkeun84@tamu.edu>
2018-03-30 16:30:38 -0500

    modify wrong log folder

--- McSim/run_dfly_ddr.sh
+++ McSim/run_dfly_ddr.sh
@@ -3,11 +3,11 @@ mkdir -p dram_log
 # benchmarks
 for bench in backprop lud pagerank spmv sgemm
 do
-  ./mcsim -mdfile ../Apps/md/md-16o3core-mesh-ddr.py -runfile ../Apps/list/run-${bench}.py > dfly_log/ddr_${bench}.log 2>&1 &
+  ./mcsim -mdfile ../Apps/md/md-16o3core-mesh-ddr.py -runfile ../Apps/list/run-${bench}.py > dram_log/ddr_${bench}.log 2>&1 &
 done
 
 # microbenchmarks
 for microbench in reduce rand_reduce mac rand_mac
 do
-  ./mcsim -mdfile ../Apps/md/md-16o3core-mesh-ddr.py -runfile ../Apps/list/run-${microbench}.py > dfly_log/ddr_${microbench}.log 2>&1 &
+  ./mcsim -mdfile ../Apps/md/md-16o3core-mesh-ddr.py -runfile ../Apps/list/run-${microbench}.py > dram_log/ddr_${microbench}.log 2>&1 &
 done
