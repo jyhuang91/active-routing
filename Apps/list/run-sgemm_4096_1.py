a94f677ea3dcb0616c90d86174fdd9b904e5e8dc
ksungkeun84 <ksungkeun84@gmail.com>
2018-03-29 22:14:28 -0400

    add list file for lud and sgemm

---
+++ Apps/list/run-sgemm_4096_1.py
@@ -0,0 +1,4 @@
+# num_threads  num_skip_1st_instrs  run_roi  dir  prog_n_argv
+# run_roi is true, ROI Hooks needs to instrumented to capture the ROI
+16 0 true ../Apps/benchmarks/baseline/sgemm ./sgemm -t 1
+
