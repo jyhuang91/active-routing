b1b04097b8ab3ac2ef99262379d16bee76167d0c
ksungkeun84 <ksungkeun84@tamu.edu>
2018-04-05 12:23:01 -0400

    add new parameter for sgemm

--- Apps/list/run-sgemm_4096_1.py
+++ Apps/list/run-sgemm_4096_1.py
@@ -1,3 +1,3 @@
 # num_threads  num_skip_1st_instrs  run_roi  dir  prog_n_argv
 # run_roi is true, ROI Hooks needs to instrumented to capture the ROI
-16 0 true ../Apps/benchmarks/baseline/sgemm ./sgemm -i 4096,4096,4096 -n 16 -s 1 4
+16 0 true ../Apps/benchmarks/baseline/sgemm ./sgemm -i 4096,4096,4096 -n 16 -s 1 -t 4
