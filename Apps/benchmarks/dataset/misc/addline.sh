8a90564d8b2ced0190b5e0146272973b0a5b2758
jyhuang <jyhuang@tamu.edu>
2018-06-21 10:34:36 -0500

    Update addline.sh

--- Apps/benchmarks/dataset/misc/addline.sh
+++ Apps/benchmarks/dataset/misc/addline.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-# use example: ./addline.sh "# NodeIDs: 916428 Degree: 456 max_deg_node: 506742" file.txt
+# use example:
+#   ./addline.sh "# NodeIDs: 916428 Degree: 456 max_deg_node: 506742" file.txt
 
 line="1i$1"
 file=$2
