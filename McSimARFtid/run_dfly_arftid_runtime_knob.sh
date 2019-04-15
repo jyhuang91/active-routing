5bba39d2a84f2e2a5b828ec0400e035824939b84
jyhuang <jyhuang@tamu.edu>
2019-04-15 19:04:21 -0400

    [mcsim] merge art-tid and art-addr with art-naive in McSimART

--- McSimARFtid/run_dfly_arftid_runtime_knob.sh
+++
@@ -1,9 +0,0 @@
-bench=lud
-folder=dfly_log_runtimeknob_${bench}
-
-mkdir -p ${folder}
-# benchmarks
-for option in 0_2048 1_2048 2_2048
-do
-    ${PWD}/mcsim -mdfile ../Apps/md/md-16o3core4GHz-mesh-hmc.py -runfile ../Apps/list/run-rb-${bench}_${option}.py -benchname dfly_arftid_${bench}_${option} > ${folder}/arftid_${bench}_${option}.log 2>&1 &
-done
