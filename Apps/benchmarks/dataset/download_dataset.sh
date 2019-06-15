f4e7f352c07a39b88f1e79572b59509f61e7b972
jyhuang <jyhuang@tamu.edu>
2019-06-14 20:35:35 -0400

    [dataset] scripts for dataset download and pre-processing automation

--- Apps/benchmarks/dataset/download_dataset.sh
+++ Apps/benchmarks/dataset/download_dataset.sh
@@ -1,8 +1,38 @@
 #!/bin/sh
 
-mkdir snap
+declare -a graphs
+graphs=(web-Google) # can add more snap graphs
 
+cd misc
+make
+cd ..
+
+mkdir -p snap
 cd snap
-wget https://snap.stanford.edu/data/web-Google.txt.gz
-gunzip web-Google.txt.gz
+
+echo "Download graphs ..."
+
+for graph in ${graphs[@]};
+do
+  wget https://snap.stanford.edu/data/$graph.txt.gz
+  gunzip $graph.txt.gz
+  ../misc/parseGraph $graph.txt >> temp
+done
+
+file="./temp"
+
+echo "Start processing graphs ..."
+
+i=0
+while read -r line; do
+  graph=${graphs[$i]}.txt
+  i=$(( i + 1 ))
+  ../misc/addline.sh "$line" $graph
+  echo "$graph processed"
+done < $file
+
+echo "Graph processing finished"
+
+rm temp
+
 cd ..
