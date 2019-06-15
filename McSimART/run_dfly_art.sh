227658f4ea6cf5da3636a58789d95af05e6b9868
jyhuang <jyhuang@tamu.edu>
2019-06-14 20:46:24 -0400

    [scripts] run scripts for launching simulations

--- McSimART/run_dfly_art.sh
+++ McSimART/run_dfly_art.sh
@@ -3,11 +3,64 @@ mkdir -p dfly_log
 # benchmarks
 for bench in backprop_2097152 lud_4096_0.75_1 pagerank_web-Google spmv_4096_0.3 sgemm_4096_1
 do
-  ./mcsim -mdfile ../Apps/md/md-16o3core2GHz-mesh-hmc.py -runfile ../Apps/list/run-ar-${bench}.py -benchname dfly_art_${bench} > dfly_log/art_${bench}.log 2>&1 &
+  # naive-art
+  ./mcsim \
+    -mdfile $PWD/../Apps/md/md-16o3core2GHz-mesh-art-naive.py \
+    -runfile ../Apps/list/run-naive-art-${bench}.py \
+    -benchname dfly_naive_art_${bench} \
+    > dfly_log/naive_art_${bench}.log 2>&1 &
+
+  # static-art
+  ./mcsim \
+    -mdfile $PWD/../Apps/md/md-16o3core2GHz-mesh-art-naive.py \
+    -runfile ../Apps/list/run-art-${bench}.py \
+    -benchname dfly_static_art_${bench} \
+    > dfly_log/static_art_${bench}.log 2>&1 &
+
+  # art-tid
+  ./mcsim \
+    -mdfile $PWD/../Apps/md/md-16o3core2GHz-mesh-art-tid.py \
+    -runfile ../Apps/list/run-art-${bench}.py \
+    -benchname dfly_art_tid_${bench} \
+    > dfly_log/art_tid_${bench}.log 2>&1 &
+
+  # art-addr
+  ./mcsim \
+    -mdfile $PWD/../Apps/md/md-16o3core2GHz-mesh-art-addr.py \
+    -runfile ../Apps/list/run-art-${bench}.py \
+    -benchname dfly_art_addr_${bench} \
+    > dfly_log/art_addr_${bench}.log 2>&1 &
 done
+
 
 # microbenchmarks
 for microbench in reduce rand_reduce mac rand_mac
 do
-  ./mcsim -mdfile ../Apps/md/md-16o3core2GHz-mesh-hmc.py -runfile ../Apps/list/run-ar-${microbench}.py -benchname dfly_art_${microbench} > dfly_log/art_${microbench}.log 2>&1 &
+  # naive-art
+  ./mcsim \
+    -mdfile $PWD/../Apps/md/md-16o3core2GHz-mesh-art-naive.py \
+    -runfile ../Apps/list/run-naive-art-${microbench}.py \
+    -benchname dfly_naive_art_${microbench} \
+    > dfly_log/naive_art_${microbench}.log 2>&1 &
+
+  # static-art
+  ./mcsim \
+    -mdfile $PWD/../Apps/md/md-16o3core2GHz-mesh-art-naive.py \
+    -runfile ../Apps/list/run-art-${microbench}.py \
+    -benchname dfly_static_art_${microbench} \
+    > dfly_log/static_art_${microbench}.log 2>&1 &
+
+  # art-tid
+  ./mcsim \
+    -mdfile ../Apps/md/md-16o3core2GHz-mesh-art-tid.py \
+    -runfile $PWD/../Apps/list/run-art-${microbench}.py \
+    -benchname dfly_art_tid_${microbench} \
+    > dfly_log/art_tid_${microbench}.log 2>&1 &
+
+  # art-addr
+  ./mcsim \
+    -mdfile $PWD/../Apps/md/md-16o3core2GHz-mesh-art-addr.py \
+    -runfile ../Apps/list/run-art-${microbench}.py \
+    -benchname dfly_art_addr_${microbench} \
+    > dfly_log/art_addr_${microbench}.log 2>&1 &
 done
--- McSimART/run_dfly_pei.sh
+++ McSimART/run_dfly_pei.sh
@@ -1,13 +1,12 @@
 mkdir -p dfly_log
 
-# benchmarks
 for bench in backprop_2097152 lud_4096_0.75_1 pagerank_web-Google spmv_4096_0.3 sgemm_4096_1
 do
   ./mcsim \
     -mdfile $PWD/../Apps/md/md-16o3core2GHz-mesh-hmc.py \
-    -runfile ../Apps/list/run-${bench}.py \
-    -benchname dfly_hmc_${bench} \
-    > dfly_log/hmc_${bench}.log 2>&1 &
+    -runfile ../Apps/list/run-pei-${bench}.py \
+    -benchname dfly_pei_${bench} \
+    > dfly_log/pei_${bench}.log 2>&1 &
 done
 
 # microbenchmarks
@@ -15,7 +14,7 @@ for microbench in reduce rand_reduce mac rand_mac
 do
   ./mcsim \
     -mdfile $PWD/../Apps/md/md-16o3core2GHz-mesh-hmc.py \
-    -runfile ../Apps/list/run-${microbench}.py \
-    -benchname dfly_hmc_${microbench} \
-    > dfly_log/hmc_${microbench}.log 2>&1 &
+    -runfile ../Apps/list/run-pei-${microbench}.py \
+    -benchname dfly_pei_${microbench} \
+    > dfly_log/pei_${microbench}.log 2>&1 &
 done
